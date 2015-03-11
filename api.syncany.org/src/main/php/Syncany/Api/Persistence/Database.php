71c4579cec7bdef775aee73c057d0f16ea972234
Philipp Heckel <philipp.heckel@gmail.com>
2015-03-11 00:46:03 +0000

    Move database contexts to a single file to ease administraton; further PhpDoc

--- api.syncany.org/src/main/php/Syncany/Api/Persistence/Database.php
+++ api.syncany.org/src/main/php/Syncany/Api/Persistence/Database.php
@@ -27,43 +27,82 @@ use Syncany\Api\Util\FileUtil;
  * Helper class for database access to create {@link PDO} objects and/or
  * create PDO statements from SQL resources.
  *
+ * <p>The class uses the {@link FileUtil} utility to load the file
+ * 'database/database.properties' and uses context-specific credentials
+ * to either create a PDO object or a statement.
+ *
  * @author Philipp Heckel <philipp.heckel@gmail.com>
  */
 class Database
 {
-    const CONFIG_CONTEXT = 1;
+    const CONFIG_CONTEXT = "database";
+    const CONFIG_NAME = "database";
 
-	public static function createInstance($configContext)
-	{
-		$config = self::readConfigFile($configContext);
+    /**
+     * Creates a PDO object from a given database config context. A config
+     * context is a prefix used to identify the database credentials in the
+     * database properties file.
+     *
+     * @param $configContext Database config context/prefix, e.g. plugins-write, plugins-read, ...
+     * @return \PDO The desired PDO object
+     * @throws ConfigException If the given config context is invalid or does not exist.
+     */
+    public static function createInstance($configContext)
+    {
+        $config = self::readConfigFile($configContext);
 
-		try {
-			return new \PDO($config['dsn'], $config['user'], $config['pass'], array());
-		} catch (\PDOException $e) {
-			throw new ConfigException("Cannot connect to database. Invalid credentials or database down.");
-		}
-	}
+        try {
+            return new \PDO($config['dsn'], $config['user'], $config['pass'], array());
+        } catch (\PDOException $e) {
+            throw new ConfigException("Cannot connect to database. Invalid credentials or database down.");
+        }
+    }
 
-	public static function prepareStatementFromResource($configContext, $namespaceContext, $sqlFile)
-	{
-		$database = Database::createInstance($configContext);
-		$sqlQuery = FileUtil::readResourceFile($namespaceContext, $sqlFile);
+    /**
+     * Retrieves a SQL file from the resources and creates a PDO statement from a given config context,
+     * a namespace context and a SQL file identifier. A config context is a prefix used to identify
+     * the database credentials in the database properties file. A namespace context is used to identify
+     * the folder in which the desired SQL resource file resides. And the SQL file parameter defines the
+     * exact file to retrieve from the resources.
+     *
+     * <p>Example:
+     * <pre>
+     *   $statement = Database::prepareStatementFromResource("links-read", 'Syncany\Api\Controller', "links.select-link.sql");
+     *
+     *   // Retrieves file from RESOURCES_DIR . 'Syncany/Api/Controller/links.select-link.sql'
+     *   // and creates a PDOStatement object from it.
+     * </pre>
+     *
+     * @param $configContext Database config context/prefix, e.g. plugins-write, plugins-read, ...
+     * @param $namespaceContext Namespace context, e.g. 'Syncany\Api\Controller'
+     * @param $sqlFile Specific SQL file, e.g. 'plugins.getById.sql'
+     * @return \PDOStatement The desired PDO statement
+     * @throws ConfigException If any of the given parameters is incorrect/invalid.
+     */
+    public static function prepareStatementFromResource($configContext, $namespaceContext, $sqlFile)
+    {
+        $database = Database::createInstance($configContext);
+        $sqlQuery = FileUtil::readResourceFile($namespaceContext, $sqlFile);
 
-		return $database->prepare($sqlQuery);
-	}
+        return $database->prepare($sqlQuery);
+    }
 
-	private static function readConfigFile($configName)
-	{
-		$config = FileUtil::readPropertiesFile(self::CONFIG_CONTEXT, $configName);
+    private static function readConfigFile($configName)
+    {
+        $config = FileUtil::readPropertiesFile(self::CONFIG_CONTEXT, self::CONFIG_NAME);
 
-		if (!$config) {
-			throw new ConfigException("Invalid config file. Parsing failed.");
-		}
+        if (!$config) {
+            throw new ConfigException("Invalid config file. Parsing failed.");
+        }
 
-		if (!isset($config['dsn']) || !isset($config['user']) || !isset($config['pass'])) {
-			throw new ConfigException("Invalid config file. Mandatory properties are 'dsn', 'user' and 'pass'.");
-		}
+        if (!isset($config[$configName . '.dsn']) || !isset($config[$configName . '.user']) || !isset($config[$configName . '.pass'])) {
+            throw new ConfigException("Invalid config file. Mandatory properties are 'dsn', 'user' and 'pass'.");
+        }
 
-		return $config;
-	}
+        return array(
+            'dsn' => $config[$configName . '.dsn'],
+            'user' => $config[$configName . '.user'],
+            'pass' => $config[$configName . '.pass']
+        );
+    }
 }
