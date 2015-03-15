36037be8beca4567fe4f38072bf1292b6f47d97d
Philipp Heckel <philipp.heckel@gmail.com>
2015-03-15 18:12:27 +0000

    Add Docker trigger

--- api.syncany.org/src/main/php/Syncany/Api/Task/DebAppReleaseUploadTask.php
+++ api.syncany.org/src/main/php/Syncany/Api/Task/DebAppReleaseUploadTask.php
@@ -20,44 +20,87 @@
 
 namespace Syncany\Api\Task;
 
+use Syncany\Api\Exception\Http\ServerErrorHttpException;
 use Syncany\Api\Model\TempFile;
 use Syncany\Api\Util\FileUtil;
 use Syncany\Api\Util\Log;
 use Syncany\Api\Util\RepreproUtil;
 use Syncany\Api\Util\StringUtil;
 
+/**
+ * This task processes incoming application release/snapshot Debian archives.
+ * It adds the archive to the respective Debian/APT archive and additionally
+ * copies the archive to the dist folder.
+ *
+ * <p>In addition to that, it also notifies Docker that a new archive is
+ * available.
+ *
+ * @author Philipp Heckel <philipp.heckel@gmail.com>
+ */
 class DebAppReleaseUploadTask extends AppReleaseUploadTask
 {
-	public function execute()
-	{
+    public function execute()
+    {
         Log::info(__CLASS__, __METHOD__, "Processing uploaded DEB release file ...");
 
-		$tempDirContext = "app/deb";
+        $tempDirContext = "app/deb";
 
-		$tempDir = FileUtil::createTempDir($tempDirContext);
-		$tempFile = FileUtil::writeToTempFile($this->fileHandle, $tempDir, ".deb");
+        $tempDir = FileUtil::createTempDir($tempDirContext);
+        $tempFile = FileUtil::writeToTempFile($this->fileHandle, $tempDir, ".deb");
 
-		$this->validateChecksum($tempFile);
-		$this->addToAptArchive($tempFile);
+        $this->validateChecksum($tempFile);
+        $this->addToAptArchive($tempFile);
 
-		$targetFile = $this->moveFile($tempFile);
-		$this->createLatestLink($targetFile);
+        $targetFile = $this->moveFile($tempFile);
+        $this->createLatestLink($targetFile);
 
-		FileUtil::deleteTempDir($tempDir);
-	}
+        $this->triggerDocker();
 
-	private function addToAptArchive(TempFile $debFile)
-	{
-		$codename = ($this->snapshot) ? "snapshot" : "release";
-		RepreproUtil::includeDeb($codename, $debFile);
-	}
+        FileUtil::deleteTempDir($tempDir);
+    }
 
-	protected function getLatestLinkBasename()
-	{
-		$snapshotSuffix = ($this->snapshot) ? "-snapshot" : "";
+    private function addToAptArchive(TempFile $debFile)
+    {
+        $codename = ($this->snapshot) ? "snapshot" : "release";
+        RepreproUtil::includeDeb($codename, $debFile);
+    }
 
-		return StringUtil::replace("syncany-latest{snapshot}.deb", array(
-			"snapshot" => $snapshotSuffix
-		));
-	}
+    protected function getLatestLinkBasename()
+    {
+        $snapshotSuffix = ($this->snapshot) ? "-snapshot" : "";
+
+        return StringUtil::replace("syncany-latest{snapshot}.deb", array(
+            "snapshot" => $snapshotSuffix
+        ));
+    }
+
+    private function triggerDocker()
+    {
+        $keysFile = FileUtil::readPropertiesFile("keys", "keys");
+        $triggerUrlProperty = "docker.trigger-url." . ($this->snapshot) ? "snapshot" : "release";
+
+        if (!isset($keysFile[$triggerUrlProperty])) {
+            throw new ServerErrorHttpException("Cannot find docker trigger token for property $triggerUrlProperty");
+        }
+
+        $triggerTokenUrl = $keysFile[$triggerUrlProperty];
+        $postFields = array("build" => "true");
+
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $triggerTokenUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $postFields);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 1);
+
+        Log::info(__CLASS__, __METHOD__, "Notifying Docker via $triggerTokenUrl ...");
+
+        $success = curl_exec($curl);
+
+        if (!$success) {
+            Log::warning(__CLASS__, __METHOD__, "FAILED to notify Docker. curl returned with an error.");
+        }
+    }
 }
